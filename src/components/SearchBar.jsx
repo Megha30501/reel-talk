a8f0d5bfebc47de13eec0d1d3e478c908993fa19
Megha30501 <m_patel39@u.pacific.edu>
2024-08-07 17:50:27 -0700

    Added SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <div className="search-bar flex items-center justify-center mt-6 relative">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="w-[35rem] h-14 p-2 pl-10 border rounded-md focus:outline-none text-white border-none"
+      />
+      <FaSearch className="absolute left-[21.5rem] text-white" />
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/components/TopMovies.jsx
+++ src/components/TopMovies.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProgressBar from "./ProgressBar";
+import SearchBar from "./SearchBar";
 
 const TopMovies = () => {
   return (
@@ -9,11 +10,15 @@ const TopMovies = () => {
       </div>
       <div className="heading text-center">
         <h2 className="text-2xl mt-5 mb-3">Select your top 5 movies</h2>
-        <p className="text-balance text-gray-300">
+        <p className="text-balance text-gray-300 ">
           Selecting your top 5 movies will enable us to suggest like-minded
           users and <br /> nearby communities for exciting watch parties and
           movie premiere gatherings.
         </p>
+      </div>
+
+      <div className="SearchBar">
+        <SearchBar />
       </div>
     </div>
   );
